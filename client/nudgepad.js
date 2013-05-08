9bf04af1e00c81588379888fddad23b60821718d
Breck <breck7@gmail.com>
2013-05-07 18:36:44 -0700

    Packaged

--- client/nudgepad.js
+++ client/nudgepad.js
@@ -8980,7 +8980,7 @@ nudgepad.emit = function (event, space) {
     return null
   
   nudgepad.socket.emit(event, space.toString(), function (data) {
-
+    console.log(data)
   })
 }
 
@@ -9405,6 +9405,14 @@ nudgepad.apps.develop.home = function () {
   nudgepad.apps.develop.renderExplorer()
 }
 
+nudgepad.apps.develop.import = function () {
+  nudgepad.textPrompt('Import a Site ', '', function (val) {
+    $.post('/nudgepad.import', {space : val}, function (err) {
+      nudgepad.notify('Imported. Please restart')
+    })
+  })
+}
+
 nudgepad.apps.develop.onopen = function () {
   $('.nudgepad#zip').attr('href', '/nudgepad.backup/' + nudgepad.domain + '.zip')
   if (!nudgepad.apps.develop.log)
@@ -9723,6 +9731,39 @@ nudgepad.getPageDimensions = function (page) {
   $('.deleteMe').remove()
   return box
 }
+nudgepad.apps.git = new App('git')
+
+nudgepad.apps.git.commit = function () {
+  var message = prompt('Enter your commit message')
+  nudgepad.apps.git.send("git commit -am '" + message + "'")
+}
+
+nudgepad.apps.git.init = function () {
+  nudgepad.apps.git.send('git init')
+}
+
+nudgepad.apps.git.pull = function () {
+  nudgepad.apps.git.send('git pull')
+}
+
+nudgepad.apps.git.push = function () {
+  nudgepad.apps.git.send('git push')
+}
+
+nudgepad.apps.git.send = function (command) {
+  $.post('nudgepad.exec', {command : command}, function (result) {
+    nudgepad.notify(result)
+  }, null, function (error, message) {
+    nudgepad.error(error.responseText)
+  })
+}
+
+nudgepad.apps.git.status = function () {
+  nudgepad.apps.git.send('git status')
+}
+
+
+
 /**
  * A grid object looks like:
  * radius 15
