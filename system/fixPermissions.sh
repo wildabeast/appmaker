fa3b3315eb32ad56312b0d5c595a0d266a18a1a1
Breck <breck7@gmail.com>
2013-05-12 13:40:05 -0700

    work on #33

--- system/fixPermissions.sh
+++ system/fixPermissions.sh
@@ -1,59 +1,22 @@
-fixPermissionsUbuntu ()
-{
-  cd $dataPath
-  
-  # allow everyone to execute this directory
-  sudo chmod 711 ~
-  
-  # todo, improve this. start with 0 permissions, work way toward proper.
-  
-  # proxy folder
-  # only ubuntu can go in here
-  sudo chown -R ubuntu:ubuntu proxy/
-  sudo chmod -R 700 proxy/
-  
-  # server folder
-  # ubuntu has read/write/execute
-  # sites can read files, execute directory
-  sudo chown -R ubuntu:sites server/
-  sudo chmod -R 750 server
-  sudo chmod 710 server
-  
-  
-  # client folder
-  # clients can read files, execute directory
-  sudo chown -R ubuntu:sites client/
-  sudo chmod -R 750 client
-  sudo chmod 710 client
-  
-  
-  # sites
-  # 
-  sudo chown ubuntu:sites sites
-  sudo chmod 710 sites
-  for domain in $sites
-    do
-      sudo chown -R $domain:$domain $sitesPath/$domain
-      sudo chmod -R 700 $sitesPath/$domain/
-  done
-}
-
-fixPermissionsMac ()
-{
-  for FOLDER in space spacefix
-  do
-  	echo $FOLDER
-  done
-}
-
 fixPermissions ()
 {
-  cd $dataPath
+  chmod 710 /nudgepad/
+  chmod 710 /nudgepad/active
+  chmod 710 /nudgepad/ports
+  chmod 710 /nudgepad/sites
+  chmod 700 /nudgepad/logs
+  chmod 700 /nudgepad/temp
+  chmod 700 /nudgepad/panel
+  chmod 700 /nudgepad/backup
+  
   if isMac
     then
-      fixPermissionsMac
+      echo Nothing to do on Mac
     else
-      fixPermissionsUbuntu
+      sudo chown $USER:sites /nudgepad/
+      sudo chown $USER:sites /nudgepad/active
+      sudo chown $USER:sites /nudgepad/ports
+      sudo chown $USER:sites /nudgepad/sites
   fi  
 }
 
---
+++ system/test.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+var assert = require('assert'),
+    fs = require('fs'),
+    modeToPermissions = require('mode-to-permissions');
+
+function runTest(line) {
+  console.log(line)
+  if (!line)
+    return false
+  var path = line.split(/ /)[0]
+  if (line.match(/^(.*) exists$/))
+    return assert(fs.existsSync(path), path + ' exists')
+
+  var permissions = modeToPermissions(fs.statSync(path).mode)
+  var who = line.match(/by (owner|others|group)$/)[1]
+  var action = line.match(/be (read|written|traversed|examined|executed) by /)[1]
+  if (action === 'written')
+    action = 'write'
+  if (!action.match(/(read|write)/))
+    action = 'execute'
+  
+  if (line.match(/can be/)) {
+    assert(permissions[action][who])
+  }
+  else {
+    assert(!permissions[action][who])
+  }
+
+}
+
+fs.readFileSync('tests.txt', 'utf8').split(/\n/g).forEach(runTest)
+
