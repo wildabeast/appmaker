be2399b3a2bc5e6d72f06c7c2ab951e687cfad31
Breck <breck7@gmail.com>
2013-05-15 14:41:48 -0700

    Started #132. Split code view

--- client/nudgepad.js
+++ client/nudgepad.js
@@ -9268,6 +9268,57 @@ nudgepad.apps.blog.updatePermalink = function () {
   var permalink = Permalink($('.nudgepad#title').val())
   $('.nudgepad#permalink').text('http://' + nudgepad.domain + '/' + permalink).attr('value', permalink)
 }
+nudgepad.codePanel = {}
+
+nudgepad.codePanel.livePreview = false
+nudgepad.codePanel.livePreviewTimeout = false
+nudgepad.codePanel.livePreviewStart = function () {
+  clearTimeout(nudgepad.codePanel.livePreviewTimeout)
+  nudgepad.codePanel.livePreviewTimeout = setTimeout('nudgepad.codePanel.livePreview()', 500)
+}
+nudgepad.codePanel.livePreview = function () {
+  var space = new Space($('#nudgepadCodePanel').val())
+  if (nudgepad.stage.selection.exists()) {
+   
+  } else {
+    nudgepad.pages.stage = new Page(space)
+    nudgepad.stage.render()
+  }
+}
+
+nudgepad.codePanel.close = function () {
+  $('#nudgepadCodePanel').hide()
+  $('#nudgepadStage').css({
+   'width' : '100%',
+   'left' : ''
+  })
+}
+
+nudgepad.codePanel.open = function () {
+  var textarea = $('#nudgepadCodePanel')
+  textarea.show()
+  $('#nudgepadStage').css({
+   'width' : '100%',
+   'left' : '40%'
+  })
+  if (nudgepad.stage.selection.exists())
+    textarea.val(nudgepad.stage.selection.toSpace().toString())
+  else
+    textarea.val(nudgepad.pages.stage.toString())
+  textarea.on('keyup', nudgepad.codePanel.livePreviewStart)
+  textarea.on('blur', nudgepad.stage.commit)
+  textarea.on('tap mousedown click slide slidestart slideend mouseup', function (event) {
+    event.stopPropagation()
+  })
+}
+
+
+nudgepad.codePanel.toggle = function () {
+  if ($('#nudgepadCodePanel:visible').length > 0)
+    nudgepad.codePanel.close()
+  else
+    nudgepad.codePanel.open()
+}
 // todo: add console history
 
 /**
@@ -12386,6 +12437,8 @@ nudgepad.bind_shortcuts = function () {
   var editSourceToggle = function () { ($('.selection').length ? nudgepad.stage.selection.editSource() : nudgepad.stage.editSource())}
   Events.shortcut.shortcuts['ctrl+u'] = editSourceToggle
   Events.shortcut.shortcuts['meta+u'] = editSourceToggle
+  
+  Events.shortcut.shortcuts['meta+shift+u'] = nudgepad.codePanel.toggle
   
   Events.shortcut.shortcuts['meta+e'] = nudgepad.stage.selection.editProperty
   
