8a1bea1ebca539c6529e0f4ce7a5cba83fba9e02
Breck <breck7@gmail.com>
2013-06-12 12:56:10 -0700

    install script fix

--- install/tasks/npmModules.sh
+++ install/tasks/npmModules.sh
@@ -1,12 +1,12 @@
 cd ~
 # Install npm modules
-for module in http-proxy@0.10.1 marked express speedcoach
+for module in http-proxy@0.10.1 marked express speedcoach space
   do
     sudo npm install $module
     sudo npm install -g $module
 done
 # sudo chown -R $USER ~/.npm
-for module in space scraps socket.io superagent imagemagick underscore cookie moment nodemailer request wrench async jquery utile commander mode-to-permissions
+for module in scraps socket.io superagent imagemagick underscore cookie moment nodemailer request wrench async jquery utile commander mode-to-permissions
   do
     npm install $module
 done
