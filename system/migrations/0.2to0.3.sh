4f0da364d0771fd4361b8dcfe6f3e455ca2d2464
Breck <breck7@gmail.com>
2013-06-21 11:10:20 -0700

    Sites to Projects migration

---
+++ system/migrations/0.2to0.3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create projects group
+sudo groupadd projects
+sudo usermod -a -G projects ubuntu
+sudo mv /nudgepad/sites /nudgepad/projects
+sudo mv /nudgepa/active /nudgepad/running
+projects="$(ls /nudgepad/projects/)"
+for domain in $projects
+do
+  sudo usermod -a -G projects $domain
+done
