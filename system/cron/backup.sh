f1e760021f95160dc902530efd7caa545f1be495
Breck <breck7@gmail.com>
2013-06-24 15:37:10 -0700

    fixes

--- system/cron/backup.sh
+++ system/cron/backup.sh
@@ -2,4 +2,4 @@
 # This script leaves /nudgepad/projects perfectly in place and creates copy
 # of it in /nudgepad/backup, excluding sub git repos, logs, and temp for
 # efficiency reasons.
-sudo rsync -a /nudgepad/projects /nudgepad/backup --exclude=".git/*" --exclude=".git" --exclude="logs/*" --exclude="temp" --exclude="temp/*" --exclude="temp"
+sudo rsync -a /nudgepad/projects /nudgepad/backup --exclude=".git/*" --exclude=".git" --exclude="private/logs/*" --exclude="private/temp" --exclude="private/temp/*" --exclude="private/temp"
--- system/migrations/0.3to0.4.sh
+++ system/migrations/0.3to0.4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 projects="$(ls /nudgepad/projects/)"
-sudo mkdir /nudgepad/backup/0.3/
-sudo cp -R /nudgepad/projects/ /nudgepad/backup/0.3/
+#sudo mkdir /nudgepad/backup/0.3/
+#sudo cp -R /nudgepad/projects/ /nudgepad/backup/0.3/
 for domain in $projects
 do
   # rename workers folder to makers folder
