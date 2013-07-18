1d4e8adf04c3fd2a43321cdaee0b4eb1b77507ae
Breck <breck7@gmail.com>
2013-07-18 12:56:42 -0700

    Fix

--- client/tools/prototype/js/ComponentsRibbon.js
+++ client/tools/prototype/js/ComponentsRibbon.js
@@ -8,12 +8,15 @@ Prototype.loadComponents = function () {
   Prototype.components = new Space()
   var list = $('#PrototypeComponentsList')
   list.html('')
+  console.log('hi')
   $('.ComponentSet').each(function () {
     var path = $(this).attr('path')
     var component = new Space($(this).text())
     var thumb = $(new Scrap(path, component.get('thumb')).toHtml())
     thumb.attr('path', path)
     thumb.addClass('Component')
+    // todo: we should not need to do this
+    thumb.removeClass('scrap')
     component.delete('thumb')
     Prototype.components.set(path, component)
     list.append(thumb)
